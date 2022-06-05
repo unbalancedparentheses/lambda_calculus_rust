9e65c785bab1856f75e56754800f099c05dc4ccf
unbalancedparentheses <mail@fcarrone.com>
2022-06-05 14:44:51 -0300

    Add parentheses

--- src/main.rs
+++ src/main.rs
@@ -50,34 +50,38 @@ fn main() {
 }
 
 fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
-	let int = text::int(10)
-	    .map(|s: String| Expr::Num(s.parse().unwrap()))
-	    .padded();
+    recursive(|expr| {
+        let int = text::int(10)
+            .map(|s: String| Expr::Num(s.parse().unwrap()))
+            .padded();
 
-    let atom = int;
+        let atom = int
+            .or(expr.delimited_by(just('('), just(')'))).padded();
 
-    let op = |c| just(c).padded();
+        let op = |c| just(c).padded();
 
-    let unary = op('-')
-        .repeated()
-        .then(atom)
-        .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
+        let unary = op('-')
+            .repeated()
+            .then(atom)
+            .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
 
-    let product = unary.clone()
-        .then(op('*').to(Expr::Mul as fn(_, _) -> _)
-        .or(op('/').to(Expr::Div as fn(_, _) -> _))
-        .then(unary)
-        .repeated())
+        let product = unary.clone()
+            .then(op('*').to(Expr::Mul as fn(_, _) -> _)
+            .or(op('/').to(Expr::Div as fn(_, _) -> _))
+            .then(unary)
+            .repeated())
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+        let sum = product.clone()
+        .then(op('+').to(Expr::Add as fn(_, _) -> _)
+            .or(op('-').to(Expr::Sub as fn(_, _) -> _))
+            .then(product)
+            .repeated())
         .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
 
-    let sum = product.clone()
-    .then(op('+').to(Expr::Add as fn(_, _) -> _)
-        .or(op('-').to(Expr::Sub as fn(_, _) -> _))
-        .then(product)
-        .repeated())
-    .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
-
-	sum.then_ignore(end())
+        sum
+    })
+    .then_ignore(end())
 }
 
 fn eval(expr: &Expr) -> Result<f64, String> {
