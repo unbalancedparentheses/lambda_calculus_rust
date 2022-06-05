8f43a400bdf1e506f85faf477f8effd316d7bf0c
unbalancedparentheses <mail@fcarrone.com>
2022-06-05 17:56:49 -0300

    WIP

--- src/main.rs
+++ src/main.rs
@@ -29,16 +29,15 @@ enum Expr {
 }
 
 fn main() {
-    //let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
-    loop {
-    print!("> ");
-    stdout().flush();
-    let mut src = String::new();
-    stdin().read_line(&mut src).unwrap();
-
+    let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
+    //loop {
+    //print!("> ");
+    //stdout().flush();
+    //let mut src = String::new();
+    //stdin().read_line(&mut src).unwrap();
 
     match parser().parse(src) {
-        Ok(ast) => match eval(&ast) {
+        Ok(ast) => match eval(&ast, &mut Vec::new()) {
             Ok(output) => println!("{}", output),
             Err(eval_err) => println!("Evaluation error: {}", eval_err),
         },
@@ -46,11 +45,14 @@ fn main() {
             .into_iter()
             .for_each(|e| println!("Parse error: {}", e)),
     }
-        }
+    //}
 }
 
 fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
-    recursive(|expr| {
+    let ident = text::ident()
+        .padded();
+
+    let expr = recursive(|expr| {
         let int = text::int(10)
             .map(|s: String| Expr::Num(s.parse().unwrap()))
             .padded();
@@ -80,18 +82,49 @@ fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
         .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
 
         sum
-    })
-    .then_ignore(end())
+    });
+
+    let decl = recursive(|decl| {
+        let r#let = text::keyword("let")
+            .ignore_then(ident)
+            .then_ignore(just('='))
+            .then(expr.clone())
+            .then_ignore(just(';'))
+            .then(decl)
+            .map(|((name, rhs), then)| Expr::Let {
+                name,
+                rhs: Box::new(rhs),
+                then: Box::new(then),
+            });
+
+        r#let
+            .or(expr)
+            .padded()
+    });
+
+    decl.then_ignore(end())
 }
 
-fn eval(expr: &Expr) -> Result<f64, String> {
+fn eval<'a>(expr: &'a Expr, vars: &mut Vec<(&'a String, f64)>) -> Result<f64, String> {
     match expr {
         Expr::Num(x) => Ok(*x),
-        Expr::Neg(a) => Ok(-eval(a)?),
-        Expr::Add(a, b) => Ok(eval(a)? + eval(b)?),
-        Expr::Sub(a, b) => Ok(eval(a)? - eval(b)?),
-        Expr::Mul(a, b) => Ok(eval(a)? * eval(b)?),
-        Expr::Div(a, b) => Ok(eval(a)? / eval(b)?),
+        Expr::Neg(a) => Ok(-eval(a, vars)?),
+        Expr::Add(a, b) => Ok(eval(a, vars)? + eval(b, vars)?),
+        Expr::Sub(a, b) => Ok(eval(a, vars)? - eval(b, vars)?),
+        Expr::Mul(a, b) => Ok(eval(a, vars)? * eval(b, vars)?),
+        Expr::Div(a, b) => Ok(eval(a, vars)? / eval(b, vars)?),
+        Expr::Var(name) => if let Some((_, val)) = vars.iter().rev().find(|(var, _)| *var == name) {
+            Ok(*val)
+        } else {
+            Err(format!("Cannot find variable {} in scope", name))
+        },
+        Expr::Let { name, rhs, then } => {
+            let rhs = eval(rhs, vars)?;
+            vars.push((name, rhs));
+            let output = eval(then, vars);
+            vars.pop();
+            output
+        },
         _ => todo!(),
     }
 }
