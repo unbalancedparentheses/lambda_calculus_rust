0107d8460e4f128b6ed326fc86b010f4d68772f6
unbalancedparentheses <mail@fcarrone.com>
2022-06-05 18:02:53 -0300

    Cargo format

--- src/main.rs
+++ src/main.rs
@@ -29,28 +29,27 @@ enum Expr {
 }
 
 fn main() {
-    let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
-    //loop {
-    //print!("> ");
-    //stdout().flush();
-    //let mut src = String::new();
-    //stdin().read_line(&mut src).unwrap();
+    //let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
+    loop {
+        print!("> ");
+        stdout().flush();
+        let mut src = String::new();
+        stdin().read_line(&mut src).unwrap();
 
-    match parser().parse(src) {
-        Ok(ast) => match eval(&ast, &mut Vec::new()) {
-            Ok(output) => println!("{}", output),
-            Err(eval_err) => println!("Evaluation error: {}", eval_err),
-        },
-        Err(parse_errors) => parse_errors
-            .into_iter()
-            .for_each(|e| println!("Parse error: {}", e)),
+        match parser().parse(src) {
+            Ok(ast) => match eval(&ast, &mut Vec::new()) {
+                Ok(output) => println!("{}", output),
+                Err(eval_err) => println!("Evaluation error: {}", eval_err),
+            },
+            Err(parse_errors) => parse_errors
+                .into_iter()
+                .for_each(|e| println!("Parse error: {}", e)),
+        }
     }
-    //}
 }
 
 fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
-    let ident = text::ident()
-        .padded();
+    let ident = text::ident().padded();
 
     let expr = recursive(|expr| {
         let int = text::int(10)
@@ -68,19 +67,27 @@ fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
             .then(atom)
             .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
 
-        let product = unary.clone()
-            .then(op('*').to(Expr::Mul as fn(_, _) -> _)
-            .or(op('/').to(Expr::Div as fn(_, _) -> _))
-            .then(unary)
-            .repeated())
+        let product = unary
+            .clone()
+            .then(
+                op('*')
+                    .to(Expr::Mul as fn(_, _) -> _)
+                    .or(op('/').to(Expr::Div as fn(_, _) -> _))
+                    .then(unary)
+                    .repeated(),
+            )
             .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
 
-        let sum = product.clone()
-        .then(op('+').to(Expr::Add as fn(_, _) -> _)
-            .or(op('-').to(Expr::Sub as fn(_, _) -> _))
-            .then(product)
-            .repeated())
-        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+        let sum = product
+            .clone()
+            .then(
+                op('+')
+                    .to(Expr::Add as fn(_, _) -> _)
+                    .or(op('-').to(Expr::Sub as fn(_, _) -> _))
+                    .then(product)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
 
         sum
     });
@@ -98,9 +105,7 @@ fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
                 then: Box::new(then),
             });
 
-        r#let
-            .or(expr)
-            .padded()
+        r#let.or(expr).padded()
     });
 
     decl.then_ignore(end())
@@ -114,18 +119,20 @@ fn eval<'a>(expr: &'a Expr, vars: &mut Vec<(&'a String, f64)>) -> Result<f64, St
         Expr::Sub(a, b) => Ok(eval(a, vars)? - eval(b, vars)?),
         Expr::Mul(a, b) => Ok(eval(a, vars)? * eval(b, vars)?),
         Expr::Div(a, b) => Ok(eval(a, vars)? / eval(b, vars)?),
-        Expr::Var(name) => if let Some((_, val)) = vars.iter().rev().find(|(var, _)| *var == name) {
-            Ok(*val)
-        } else {
-            Err(format!("Cannot find variable {} in scope", name))
-        },
+        Expr::Var(name) => {
+            if let Some((_, val)) = vars.iter().rev().find(|(var, _)| *var == name) {
+                Ok(*val)
+            } else {
+                Err(format!("Cannot find variable {} in scope", name))
+            }
+        }
         Expr::Let { name, rhs, then } => {
             let rhs = eval(rhs, vars)?;
             vars.push((name, rhs));
             let output = eval(then, vars);
             vars.pop();
             output
-        },
+        }
         _ => todo!(),
     }
 }
